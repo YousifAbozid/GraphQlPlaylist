9113faa67d4beeec1e6f7e58409d7cdd97921d2c
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-16 23:16:21 +0200

    Setting up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
+import BookList from './components/BookList'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+})
 
 const App = () => {
   return (
-    <div>
-      <h1>Yousif's Book List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Yousif's Book List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   )
 }
 
