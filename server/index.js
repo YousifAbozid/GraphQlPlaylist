dde9f51ceaa419ccc3591b09559a4e61b843ae48
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-16 14:55:22 +0200

    Connect the app to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 const { graphqlHTTP } = require('express-graphql')
 import schema from './models/schema.js'
 
-const app = express()
 dotenv.config()
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => console.log('connected to MongoDB'))
+    .catch((error) => {
+        console.log('can\'t connect to MongoDB')
+        console.log(error.message)
+    })
+
+const app = express()
 
 app.use('/graphql', graphqlHTTP({
     schema,
